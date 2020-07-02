020ce165fef4e63fbae92c10effc22ecf187e693
karthik.nish <karthik.nishanth06@gmail.com>
2020-07-02 22:56:24 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+        consequatur quis facere omnis totam inventore in, tempora libero maiores
+        perferendis sunt ex modi doloribus non iure, vero ipsa suscipit numquam.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe fugit
+        atque libero nostrum maiores iure, excepturi laboriosam suscipit fugiat
+        reiciendis. Id corrupti odit fuga, repellendus quasi dignissimos fugiat
+        quia maxime. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Vero accusantium asperiores molestiae id dicta laboriosam iure, cumque
+        fuga ad ullam! Velit at voluptatem vitae vel eveniet sint minima est
+        nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repellendus impedit facere beatae deserunt iste dolores itaque quia
+        obcaecati, fugit temporibus expedita officiis cupiditate culpa aut. Ipsa
+        ipsam dolore nemo dolorum.
+      </p>
+    </section>
+  );
+}
